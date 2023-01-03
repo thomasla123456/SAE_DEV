8986c7b9dd6686a11a798a8cc7c92388ab4ed243
Thomas David <thomas.david@etu.univ-smb.fr>
2023-01-03 15:28:05 +0100

    lecode

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.TextureAtlases;
 
 namespace Project1
 {
@@ -8,8 +10,10 @@ namespace Project1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private int t = 200;
-        private int tet = 11;
+        private Texture2D background;
+        private int hauteurFenetre = 988;
+        private int longueuFenetre = 745;
+        private Rectangle bgRect;
 
 
         public Game1()
@@ -17,12 +21,12 @@ namespace Project1
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+          /* _graphics.ToggleFullScreen();  quand on sera chaud */
         }
 
         protected override void Initialize()
         {
             Window.Title = "Test";
-            // TODO: Add your initialization logic here
 
             base.Initialize();
         }
@@ -30,8 +34,10 @@ namespace Project1
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            background = Content.Load<Texture2D>("Texutres/bg");
+            bgRect = new Rectangle(0, 0, _graphics.GraphicsDevice.Viewport.Width , _graphics.GraphicsDevice.Viewport.Height);  
             // TODO: use this.Content to load your game content here
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -47,9 +53,9 @@ namespace Project1
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-            GraphicsDevice.Clear(Color.Red);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background,bgRect,Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
