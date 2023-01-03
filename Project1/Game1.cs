acc3d4839d5966e3b294ef10061662ec77c65fcf
Thomas David <thomas.david@etu.univ-smb.fr>
2023-01-03 14:20:53 +0100

    tet

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -9,6 +9,7 @@ namespace Project1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private int t = 200;
+        private int tet = 1;
 
 
         public Game1()
