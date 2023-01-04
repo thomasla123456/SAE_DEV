7169a6b7b1d27337f30b774b293b96bc6cbdc6dd
Thomas David <thomas.david@etu.univ-smb.fr>
2023-01-04 15:14:18 +0100

    CU

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -11,13 +11,19 @@ namespace Project1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D background;
+        private Rectangle bgRect;
+
         private int hauteurFenetre = 900;
         private int longueurFenetre = 1600;
-        private Rectangle bgRect;
 
         private Vector2 textPos;
         private SpriteFont textFont;
         private const string textMsg =" dans le jeu";
+
+        private Texture2D playerText;
+        private Rectangle playerRect;
+        private KeyboardState kb;
+        private const int vitesse = 5;
 
         public Game1()
         {
@@ -34,7 +40,7 @@ namespace Project1
             _graphics.PreferredBackBufferWidth = longueurFenetre;
            _graphics.ApplyChanges();    
             Window.AllowUserResizing = false;
-
+            Window.AllowAltF4 = true;
             base.Initialize();
         }
 
@@ -45,6 +51,8 @@ namespace Project1
             textFont = Content.Load<SpriteFont>("Fonts/TextFont");
             bgRect = new Rectangle(0, 0, _graphics.GraphicsDevice.Viewport.Width , _graphics.GraphicsDevice.Viewport.Height);
             textPos = new Vector2(0,10);
+            playerText = Content.Load<Texture2D>("Texutres/perso");
+            playerRect = new Rectangle(500,500,playerText.Width,playerText.Height);
 
         }
 
@@ -53,7 +61,17 @@ namespace Project1
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+           kb = Keyboard.GetState();
+
+            if (kb.IsKeyDown(Keys.Z))
+                playerRect.Y -= vitesse;
+            else if (kb.IsKeyDown(Keys.S))
+                playerRect.Y += vitesse;
+
+            if (kb.IsKeyDown(Keys.D))
+                playerRect.X += vitesse;
+            else if (kb.IsKeyDown(Keys.Q))
+                playerRect.X -= vitesse;
 
             base.Update(gameTime);
         }
@@ -63,6 +81,7 @@ namespace Project1
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
             _spriteBatch.Draw(background,bgRect,Color.White);
+            _spriteBatch.Draw(playerText,playerRect,Color.White);
             _spriteBatch.DrawString(textFont, textMsg, textPos, Color.Black);
             _spriteBatch.End();
 
